c4f68bf326efefab7f50c1c4a2175e636233f61f
Your Namecls <you@example.com>
2024-05-29 00:12:23 -0300

    cambio en create reservation

--- lib/screens/cliente/calander.dart
+++ lib/screens/cliente/calander.dart
@@ -3,19 +3,20 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RepairRequestCalendar extends StatefulWidget {
-  final Function(DateTime)? onDateTimeSelected;
+  final Function(String)? onReservationIdSelected;
 
-  const RepairRequestCalendar({Key? key, this.onDateTimeSelected}) : super(key: key);
+  const RepairRequestCalendar({Key? key, this.onReservationIdSelected})
+      : super(key: key);
 
   @override
-  _SelectDateTimeScreenState createState() => _SelectDateTimeScreenState();
+  _RepairRequestCalendarState createState() => _RepairRequestCalendarState();
 }
 
-class _SelectDateTimeScreenState extends State<RepairRequestCalendar> {
+class _RepairRequestCalendarState extends State<RepairRequestCalendar> {
   late DateTime _selectedDate;
   late TimeOfDay _selectedTime;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final Map<DateTime, List<TimeOfDay>> _reservedTimes = {};
+  Map<DateTime, List<TimeOfDay>> _reservedTimes = {};
 
   @override
   void initState() {
@@ -59,8 +60,8 @@ class _SelectDateTimeScreenState extends State<RepairRequestCalendar> {
       onDaySelected: (selectedDay, focusedDay) {
         setState(() {
           _selectedDate = selectedDay;
-          _fetchReservedTimes(_selectedDate);
         });
+        _fetchReservedTimes(selectedDay);
       },
     );
   }
@@ -72,62 +73,59 @@ class _SelectDateTimeScreenState extends State<RepairRequestCalendar> {
         itemBuilder: (context, index) {
           final hour = index + 9;
           final time = TimeOfDay(hour: hour, minute: 0);
-          final dateTime = DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day, hour, 0);
+          final dateTime = DateTime(_selectedDate.year, _selectedDate.month,
+              _selectedDate.day, hour, 0);
 
-          if (_isTimeAvailable(dateTime)) {
-            return ListTile(
-              title: Text(time.format(context)),
-              onTap: () {
-                setState(() {
-                  _selectedTime = time;
-                });
-              },
-              selected: _selectedTime == time,
-            );
-          } else {
-            return const SizedBox(); // No mostrar horario si está reservado
-          }
+          return ListTile(
+            title: Text(time.format(context)),
+            onTap: _isTimeAvailable(dateTime)
+                ? () {
+                    setState(() {
+                      _selectedTime = time;
+                    });
+                  }
+                : null,
+            selected: _selectedTime == time,
+            enabled: _isTimeAvailable(dateTime),
+          );
         },
       ),
     );
   }
 
   bool _isTimeAvailable(DateTime dateTime) {
-    final times = _reservedTimes[DateTime(dateTime.year, dateTime.month, dateTime.day)];
+    final times =
+        _reservedTimes[DateTime(dateTime.year, dateTime.month, dateTime.day)];
     if (times != null) {
-      final selectedTime = TimeOfDay(hour: dateTime.hour, minute: dateTime.minute);
-      return !times.contains(selectedTime);
+      final selectedTime =
+          TimeOfDay(hour: dateTime.hour, minute: dateTime.minute);
+      for (var reservedTime in times) {
+        if (reservedTime.hour == selectedTime.hour &&
+            reservedTime.minute == selectedTime.minute) {
+          return false; // La hora seleccionada ya está reservada
+        }
+      }
     }
-    return true;
+    return true; // La hora seleccionada está disponible
   }
 
-  void _fetchReservedTimes(DateTime date) async {
+  Future<void> _fetchReservedTimes(DateTime date) async {
     try {
       final snapshot = await _firestore
           .collection('reservations')
-          .doc('${date.year}-${date.month}-${date.day}')
-          .collection('times')
-          .where('reserved', isEqualTo: true)
+          .where('date', isGreaterThanOrEqualTo: Timestamp.fromDate(date))
+          .where('date',
+              isLessThan: Timestamp.fromDate(date.add(const Duration(days: 1))))
+          .where('reserved',
+              isEqualTo: true) // Filtra solo las reservas confirmadas
           .get();
 
       final times = snapshot.docs.map((doc) {
         final data = doc.data();
-        if (data.containsKey('time') && data['time'] is String) {
-          final timeString = data['time'] as String;
-          final timeParts = timeString.split(':');
-          if (timeParts.length == 2) {
-            final hour = int.parse(timeParts[0]);
-            final minute = int.parse(timeParts[1]);
-            return TimeOfDay(hour: hour, minute: minute);
-          } else {
-            print("El campo 'time' del documento ${doc.id} no tiene el formato correcto.");
-            return null;
-          }
-        } else {
-          print("El documento ${doc.id} no tiene un campo 'time' válido.");
-          return null;
-        }
-      }).where((time) => time != null).cast<TimeOfDay>().toList();
+        final timestamp = data['date'] as Timestamp;
+        final dateTime = timestamp.toDate();
+        return TimeOfDay(hour: dateTime.hour, minute: dateTime.minute);
+      }).toList();
 
       setState(() {
         _reservedTimes[DateTime(date.year, date.month, date.day)] = times;
@@ -138,9 +136,34 @@ class _SelectDateTimeScreenState extends State<RepairRequestCalendar> {
     }
   }
 
-  void _handleDateTimeSelection() {
-    final selectedDateTime = DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day, _selectedTime.hour, _selectedTime.minute);
-    widget.onDateTimeSelected?.call(selectedDateTime);
+  Future<void> _handleDateTimeSelection() async {
+    final selectedDateTime = DateTime(_selectedDate.year, _selectedDate.month,
+        _selectedDate.day, _selectedTime.hour, _selectedTime.minute);
+
+    // Verifica si la fecha y hora seleccionadas ya están reservadas
+    if (!_isTimeAvailable(selectedDateTime)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content:
+                Text('La fecha y hora seleccionadas ya están reservadas.')),
+      );
+      return;
+    }
+
+    // Crear la reserva en Firestore y obtener el ID
+    DocumentReference reservationRef =
+        await _firestore.collection('reservations').add({
+      'date': selectedDateTime,
+      'reserved': false, // Inicialmente falso
+      // Aquí debes agregar cualquier otro campo necesario, como userId, vehicleId, etc.
+    });
+
+    String reservationId = reservationRef.id;
+
+    // Enviar el reservationId de vuelta a SelectService
+    widget.onReservationIdSelected?.call(reservationId);
+
+    // Volver a la pantalla anterior
     Navigator.pop(context);
   }
 }

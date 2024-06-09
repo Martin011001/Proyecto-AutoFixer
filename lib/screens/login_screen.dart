12e56d298777a2f0eb005246129a87d98c387dfa
pedroKahane <pedrok@datosclaros.com>
2024-06-09 16:55:02 -0300

    hotfix

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -67,7 +67,6 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(

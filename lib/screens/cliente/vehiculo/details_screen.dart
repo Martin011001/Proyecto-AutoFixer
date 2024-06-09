99a91a1e065f6576dfd5ef09c82d4478b69ce6aa
Your Namecls <you@example.com>
2024-06-09 12:58:42 -0300

    cambios en screen details vehiculo y acciones

--- lib/screens/cliente/vehiculo/details_screen.dart
+++ lib/screens/cliente/vehiculo/details_screen.dart
@@ -9,29 +9,36 @@ class VehicleDetailsScreen extends StatelessWidget {
 
   const VehicleDetailsScreen({super.key, required this.vehiculo});
 
- Future<void> eliminarVehiculo(BuildContext context) async {
+  Future<void> eliminarVehiculo(BuildContext context) async {
     try {
-      // Inicia una transacción para asegurar la consistencia de la eliminación
       await FirebaseFirestore.instance.runTransaction((transaction) async {
         // Referencia al documento del vehículo
-        DocumentReference vehicleRef = FirebaseFirestore.instance.collection('vehiculos').doc(vehiculo.id);
+        DocumentReference vehicleRef =
+            FirebaseFirestore.instance.collection('vehiculos').doc(vehiculo.id);
 
-
-        // Obtén los turnos asociados al vehicleId del vehículo
-        QuerySnapshot turnosSnapshot = await FirebaseFirestore.instance.collection('turns')
-            .where('vehicleId', isEqualTo: vehiculo.id).get();
+        // turnos asociados al vehicleId del vehículo
+        QuerySnapshot turnosSnapshot = await FirebaseFirestore.instance
+            .collection('turns')
+            .where('vehicleId', isEqualTo: vehiculo.id)
+            .get();
         for (DocumentSnapshot turnoDoc in turnosSnapshot.docs) {
-          // Elimina cada turno
           transaction.delete(turnoDoc.reference);
         }
-         // Elimina el vehículo
+        // Elimina el vehículo
         transaction.delete(vehicleRef);
       });
 
       // Redirige a la página principal después de eliminar
+      // ignore: use_build_context_synchronously
       context.go('/cliente/vehiculo/list'); // Ajusta la ruta según tu configuración
+
+      // Mostrar mensaje de éxito
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Vehículo eliminado exitosamente')),
+      );
     } catch (e) {
-      // Maneja el error si ocurre
+      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error al eliminar el vehículo: $e')),
       );
@@ -45,31 +52,92 @@ class VehicleDetailsScreen extends StatelessWidget {
         title: const Text('Detalles del Auto'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Modelo: ${vehiculo.model}'),
-            Text('Marca: ${vehiculo.brand}'),
-            Text('Patente: ${vehiculo.licensePlate}'),
-            Text('Año: ${vehiculo.year ?? 'Desconocido'}'),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    context.push('/cliente/vehiculo/edit', extra: vehiculo);
-                  },
-                  child: const Text('Editar'),
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(  
-                  onPressed: () => eliminarVehiculo(context),
-                  child: const Text('Eliminar'),
-                ),
-              ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            elevation: 8.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
             ),
-          ],
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '${vehiculo.brand} ${vehiculo.model}',
+                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Modelo: ${vehiculo.model}',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    'Marca: ${vehiculo.brand}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Patente: ${vehiculo.licensePlate}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Año: ${vehiculo.year ?? 'Desconocido'}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          context.push('/cliente/vehiculo/edit', extra: vehiculo);
+                        },
+                        child: const Text('Editar'),
+                      ),
+                      const SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          // Mostrar diálogo de confirmación
+                          bool confirmacion = await showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Confirmar Eliminación'),
+                                content: const Text('¿Estás seguro de que deseas eliminar este vehículo?'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop(false); // No elimina el vehículo
+                                    },
+                                    child: const Text('Cancelar'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop(true); // Confirma la eliminación del vehículo
+                                    },
+                                    child: const Text('Eliminar'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+
+                          // Si la confirmación es true, elimina el vehículo
+                          if (confirmacion == true) {
+                            // ignore: use_build_context_synchronously
+                            await eliminarVehiculo(context);
+                          }
+                        },
+                        child: const Text('Eliminar'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );

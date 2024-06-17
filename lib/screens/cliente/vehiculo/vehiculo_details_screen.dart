1d17237225fb0e5831eaffc55e46a55a367bbf81
pedroKahane <pedrok@datosclaros.com>
2024-06-17 12:13:26 -0300

    fix

--- lib/screens/cliente/vehiculo/vehiculo_details_screen.dart
+++ lib/screens/cliente/vehiculo/vehiculo_details_screen.dart
@@ -1,4 +1,3 @@
-import 'package:aplicacion_taller/entities/info_cliente_turn_progress.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
